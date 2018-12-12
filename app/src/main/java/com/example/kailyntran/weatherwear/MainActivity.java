d98bd4ca152f84bb7de8065d64e454d6142f3c77
rshah99 <45664105+rshah99@users.noreply.github.com>
2018-12-11 22:35:35 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/kailyntran/weatherwear/MainActivity.java
+++ app/src/main/java/com/example/kailyntran/weatherwear/MainActivity.java
@@ -1,131 +1,3 @@
-//package com.example.kailyntran.weatherwear;
-//
-//import android.content.Intent;
-//import android.app.Activity;
-//import android.os.Bundle;
-//import android.util.Log;
-//import android.os.AsyncTask;
-//import android.view.View;
-//import android.widget.EditText;
-//import android.view.View.OnClickListener;
-//import java.io.BufferedReader;
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//import android.widget.Button;
-//import android.widget.Toast;
-//
-//import com.android.volley.Request;
-//import com.android.volley.RequestQueue;
-//import com.android.volley.Response;
-//import com.android.volley.VolleyError;
-//import com.android.volley.toolbox.StringRequest;
-//import com.android.volley.toolbox.Volley;
-//
-//public class MainActivity extends Activity {
-//
-//    private static final String TAG_DEBUG = MainActivity.class.getName();
-//    public static final String TAG_ID = "id";
-//    public static final String TAG_CURRENTTEMP = "currenttemp";
-//    public static final String TAG_MAXTEMP = "maxtemp";
-//    public static final String TAG_MINTEMP = "mintemp";
-//    public static final String TAG_ZIP = "zip";
-//
-//    private String zip = "";
-//    private String id = "";
-//    private String currenttemp = "";
-//    private String maxtemp = "";
-//    private String mintemp = "";
-//
-//    private EditText enteredzip;
-//
-//    private Button button;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        enteredzip = (EditText) findViewById(R.id.editText);
-//
-//        button = (Button) findViewById(R.id.button);
-//
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                zip = enteredzip.getText().toString();
-//                String BASE_URL = "https://api.openweathermap.org/data/2.5/weather?zip=";
-//                String API_CALL= "&APPID=9974c0ebd73bf2310ea2bcd73c09e316";
-//
-//                //Request a string response from the URL resource
-//                StringRequest stringRequest = new StringRequest(Request.Method.GET, BASE_URL+zip+API_CALL,
-//                        new Response.Listener<String>() {
-//                            @Override
-//                            public void onResponse(String response) {
-//                                // Display the response string.
-//                                //Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
-//                                for (int i = 0; i < response.length(); i++) {
-//                                    if (response.substring(i, i + 2).equals("id")) {
-//                                        id = response.substring(i + 4, i + 7);
-//                                        break;
-//                                    }
-//                                }
-//
-//                                for (int i = 0; i < response.length(); i++) {
-//                                    if (response.substring(i, i + 4).equals("temp")) {
-//                                        currenttemp = response.substring(i + 6, i + 9);
-//                                        break;
-//                                    }
-//                                }
-//
-//                                for (int i = 0; i < response.length(); i++) {
-//                                    if (response.substring(i, i + 8).equals("temp_min")) {
-//                                        mintemp = response.substring(i + 10, i + 13);
-//                                        break;
-//                                    }
-//                                }
-//
-//                                for (int i = 0; i < response.length(); i++) {
-//                                    if (response.substring(i, i + 8).equals("temp_max")) {
-//                                        maxtemp = response.substring(i + 10, i + 13);
-//                                        break;
-//                                    }
-//                                }
-//                                String everything = "id: "+ id + " currenttemp "+ currenttemp + " maxtemp: " + maxtemp + " mintemp: " + mintemp + " zip: " + zip;
-//                                Toast.makeText(MainActivity.this, everything, Toast.LENGTH_LONG).show();
-//                                launchMain2Activity(id, currenttemp, maxtemp, mintemp);
-//                            }
-//                        }, new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
-//                        return;
-//                    }
-//                });
-//                //Instantiate the RequestQueue and add the request to the queue
-//                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-//                queue.add(stringRequest);
-//                // new RetrieveFeedTask().execute();
-//            }
-//        });
-//
-//    }
-//
-//        private void launchMain2Activity(String id, String currenttemp, String maxtemp, String mintemp) {
-//            Intent Main2Activity = new Intent(MainActivity.this, Main2Activity.class);
-//
-//            Main2Activity.putExtra(TAG_ID, id);
-//            Main2Activity.putExtra(TAG_CURRENTTEMP, currenttemp);
-//            Main2Activity.putExtra(TAG_MAXTEMP, maxtemp);
-//            Main2Activity.putExtra(TAG_MINTEMP, mintemp);
-//            Main2Activity.putExtra(TAG_ZIP, zip);
-//
-//            startActivity(Main2Activity);
-//        }
-//
-//
-//}
 
 package com.example.kailyntran.weatherwear;
 
@@ -145,44 +17,50 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends Activity {
-
+    
+    //creating tags for each variable so the tags can be passed to the next activity
     public static final String TAG_ID = "id";
     public static final String TAG_CURRENTTEMP = "currenttemp";
     public static final String TAG_MAXTEMP = "maxtemp";
     public static final String TAG_MINTEMP = "mintemp";
     public static final String TAG_ZIP = "zip";
+   
+    //variables that will be intialized in on click and passed to the next activity
     private String zip = "";
     private String id = "";
     private String currenttemp = "";
     private String maxtemp = "";
     private String mintemp = "";
-
+    
     private EditText enteredzip;
 
     private Button button;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         enteredzip = (EditText) findViewById(R.id.editText);
 
         button = (Button) findViewById(R.id.button);
-
+        
+      
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //initializes zip to the value in the EditText enteredzip
                 zip = enteredzip.getText().toString();
                 String BASE_URL = "https://api.openweathermap.org/data/2.5/weather?zip=";
                 String API_CALL= "&APPID=9974c0ebd73bf2310ea2bcd73c09e316";
 
-                //Request a string response from the URL resource
+                //Request a string response from the URL resource using the volley method
                 StringRequest stringRequest = new StringRequest(Request.Method.GET, BASE_URL+zip+API_CALL,
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
-                                // Display the response string.
+                                //Parses the response string which comes in a json format to assign values to the strings declared above
                                 for (int i = 0; i < response.length(); i++) {
                                     if (response.substring(i, i + 2).equals("id")) {
                                         id = response.substring(i + 4, i + 7);
@@ -210,12 +88,14 @@ public class MainActivity extends Activity {
                                         break;
                                     }
                                 }
-
+                        
                                 launchMain2Activity();
                             }
+                            //catches an error in the volley
                         }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        //When an error is returned from the volley request due to a given zip code not having a value in the API, this toasts an error message that prompts the user to reenter a zip code
                         Toast.makeText(MainActivity.this, getResources().getString(R.string.error_et), Toast.LENGTH_LONG).show();
                         return;
                     }
@@ -230,7 +110,8 @@ public class MainActivity extends Activity {
 
     private void launchMain2Activity() {
         Intent Main2Activity = new Intent(MainActivity.this, Main2Activity.class);
-
+        
+        //pass variables to next activity using tags
         Main2Activity.putExtra(TAG_ID, id);
         Main2Activity.putExtra(TAG_CURRENTTEMP, currenttemp);
         Main2Activity.putExtra(TAG_MAXTEMP, maxtemp);
